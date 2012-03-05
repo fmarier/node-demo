61f5564ea07dc61dd543779ec31d0030d228f035
Francois Marier <francois@debian.org>
2012-03-05 23:42:37 +1300

    Add a new register action
    This includes a simple form with server-side validation which
    reports errors to the user.
    
    It also adds a stylesheet and enables the serving of static files.
    
    Finally, the action menu on the index page is now dynamically
    created based on an array of action names.

--- index.js
+++ index.js
@@ -22,16 +22,64 @@ function readCounter(displayCounter) {
                  });
 }
 
+var usernameRegexp = new RegExp(/^[a-z0-9_]+$/);
+
+function createAccount(username, handleResult) {
+    if (username.length < 3) {
+        handleResult('Username must be at least 3 characters');
+        return;
+    }
+
+    if (!username.match(usernameRegexp)) {
+        handleResult('Username can only include letters, numbers and underscores.');
+        return;
+    }
+
+    client.query('USE nodedemo');
+    client.query("INSERT INTO accounts(username) VALUES (?)", [username], function (err, results, field) {
+                     if (err) {
+                         handleResult(err);
+                     } else {
+                         handleResult();
+                     }
+                 });
+}
+
 const app = express.createServer();
+app.use(express.limit('1mb'));
+app.use(express.bodyParser());
+app.use(express.static('static'));
+
 app.get('/', function(req, res) {
-            res.render('index.ejs', {title: 'index', layout: true});
+            res.render('index.ejs', {title: 'index', actions: ['count', 'register']});
         });
 
 app.get('/count', function(req, res) {
             readCounter(function (countValue) {
-                            res.render('count.ejs', {title: 'count', count: countValue, layout: true});
+                            res.render('count.ejs', {title: 'count', count: countValue});
                             bumpCounter();
                         });
         });
+
+app.get('/register', function(req, res) {
+            res.render('register.ejs', {title: 'register', error: false});
+        });
+
+app.post('/register', function(req, res) {
+             var username = req.body.username;
+             if (!username) {
+                 res.redirect('home');
+                 return;
+             }
+             createAccount(username, function (error) {
+                               if (error) {
+                                   res.render('register.ejs', {title: 'register', error: error});
+                               } else {
+                                   // TODO: convert to req.flash()
+                                   console.log(username + ' user account successfully created');
+                                   res.redirect('home');
+                               }
+                           });
+         });
 
 app.listen(9001);
