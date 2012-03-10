1ed584db09a50324676591d422120d135ecaefc2
Francois Marier <francois@debian.org>
2012-03-10 22:37:35 +1300

    Add schema definition for existing tables

---
+++ schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE accounts ( id INT(11) AUTO_INCREMENT, username VARCHAR(255), password VARCHAR(255), PRIMARY KEY (id));
+CREATE TABLE counter ( id INT(11) AUTO_INCREMENT, name VARCHAR(255), value INT, PRIMARY KEY (id));
