c958020b145f639d3c0300f2b861d4361cd096f8
Francois Marier <francois@debian.org>
2012-03-11 00:25:38 +1300

    Add editable profile fields to user accounts

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ var client = mysql.createClient({
   user: 'nodeuser',
   password: 'nodeuser'
 });
+client.query('USE nodedemo');
 
 function bumpCounter() {
-    client.query('USE nodedemo');
     client.query("UPDATE counter SET value = value + 1 WHERE name = 'main'");
 }
 
 function readCounter(displayCounter) {
-    client.query('USE nodedemo');
     client.query("SELECT value FROM counter WHERE name = 'main'", function (err, results, fields) {
                      if (err) {
                          throw err;
@@ -22,7 +21,65 @@ function readCounter(displayCounter) {
                  });
 }
 
-var usernameRegexp = new RegExp(/^[a-z0-9_]+$/);
+function getUserProfile(username, displayProfile) {
+    if (!username) {
+        displayProfile();
+        return;
+    }
+
+    client.query("SELECT * FROM accounts WHERE username = ?", [username], function (err, results, fields) {
+                     if (err) {
+                         throw err;
+                     }
+                     displayProfile(results[0]);
+                 });
+}
+
+var nameRegexp = new RegExp(/^[A-Za-z\-]+$/);
+var emailRegexp = new RegExp(/^.+@.+\..+$/);
+
+function updateUserProfile(username, fields, handleResult) {
+    if (username !== fields.username) {
+        handleResult('Changing your username is not allowed.');
+        return;
+    }
+
+    if ((fields.firstname && !fields.firstname.match(nameRegexp)) ||
+        (fields.lastname && !fields.lastname.match(nameRegexp))) {
+        handleResult('First and last names can only include letters and hyphens.');
+        return;
+    }
+    if (fields.email && !fields.email.match(emailRegexp)) {
+        handleResult('Invalid email address.');
+        return;
+    }
+
+    var data = [{field: "firstname", value: fields.firstname || undefined},
+                {field: "lastname", value: fields.lastname || undefined},
+                {field: "email", value: fields.email || undefined}
+               ];
+    var values = [];
+
+    var sql = 'UPDATE accounts SET ';
+    for (var i=0; i < data.length; i++) {
+        if (i > 0) {
+            sql += ', ';
+        }
+        sql += data[i].field + " = ?";
+        values.push(data[i].value);
+    }
+    sql += " WHERE username = ?";
+    values.push(username);
+
+    client.query(sql, values, function (err) {
+                     if (err) {
+                         throw err;
+                     }
+                     handleResult();
+                 });
+}
+
+var usernameRegexp = new RegExp(/^[A-Za-z0-9_]+$/);
 
 function createAccount(username, handleResult) {
     if (username.length < 3) {
@@ -35,7 +92,6 @@ function createAccount(username, handleResult) {
         return;
     }
 
-    client.query('USE nodedemo');
     client.query("INSERT INTO accounts(username) VALUES (?)", [username], function (err, results, field) {
                      if (err) {
                          handleResult(err);
@@ -51,7 +107,6 @@ function authenticateUser(username, logUserIn) {
         return;
     }
 
-    client.query('USE nodedemo');
     client.query("SELECT id FROM accounts where username = ?", [username], function (err, results, field) {
                      if (err) {
                          logUserIn(err);
@@ -74,7 +129,7 @@ app.use(express.session({ secret: "secret string in a public repo ftw!" }));
 
 app.get('/', function(req, res) {
             res.render('index.ejs', {title: 'index', info: req.flash('info'), error: req.flash('error'),
-                                     actions: ['count', 'login', 'logout', 'register'],
+                                     actions: ['count', 'login', 'logout', 'profile', 'register'],
                                      username: req.session.username});
         });
 
@@ -90,11 +145,6 @@ app.get('/login', function(req, res) {
             res.render('login.ejs', {title: 'login', info: req.flash('info'), error: req.flash('error')});
         });
 
-app.get('/logout', function(req, res) {
-            delete req.session.username;
-            res.redirect('home');
-        });
-
 app.post('/login', function(req, res) {
              var username = req.body.username;
              authenticateUser(username, function (error) {
@@ -107,6 +157,34 @@ app.post('/login', function(req, res) {
                                       res.redirect('home');
                                   }
                               });
+         });
+
+app.get('/logout', function(req, res) {
+            delete req.session.username;
+            res.redirect('home');
+        });
+
+app.get('/profile', function(req, res) {
+            getUserProfile(req.session.username, function (account) {
+                               if (account) {
+                                   res.render('profile.ejs', {title: 'profile', info: req.flash('info'), error: req.flash('error'),
+                                                              account: account});
+                               } else {
+                                   res.redirect('home');
+                               }
+                           });
+        });
+
+app.post('/profile', function(req, res) {
+             updateUserProfile(req.session.username, req.body, function (error) {
+                                  if (error) {
+                                      req.flash('error', error);
+                                      res.redirect('/profile');
+                                  } else {
+                                      req.flash("info", req.session.username + " updated");
+                                      res.redirect('home');
+                                  }
+                               });
          });
 
 app.get('/register', function(req, res) {
