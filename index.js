2e66a7b85cef311b6d1ef9a5cd1d8ddedc135716
Francois Marier <francois@debian.org>
2012-03-12 23:13:35 +1300

    Add support for uploading an image file in user profile
    Unfortunately this means switching from express' bodyParser to
    connect-form (which is dead upstream) to easily get submission of
    files through a form:
    
      https://github.com/visionmedia/express/blob/0634bf0b0dae0b005f4b459bf00c163ca2208599/examples/multipart/app.js
      http://stackoverflow.com/questions/6366463/nodejs-express-and-file-uploading
    
    Once Express 3 is out, it will work with connect 2 which uses
    node-formidable to parse form input out of the box:
    
      https://github.com/visionmedia/express/blob/master/examples/multipart/app.js
    
    and we'll be able to drop that dependency.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+#!/usr/bin/node
+
 const
+crypto = require('crypto'),
 express = require('express'),
+form = require('connect-form'),
+fs = require('fs'),
+util = require('util'),
 mysql = require('mysql');
 
 var client = mysql.createClient({
@@ -38,7 +44,7 @@ function getUserProfile(username, displayProfile) {
 var nameRegexp = new RegExp(/^[A-Za-z\-]+$/);
 var emailRegexp = new RegExp(/^.+@.+\..+$/);
 
-function updateUserProfile(username, fields, handleResult) {
+function updateUserProfile(username, fields, files, handleResult) {
     if (username !== fields.username) {
         handleResult('Changing your username is not allowed.');
         return;
@@ -52,6 +58,41 @@ function updateUserProfile(username, fields, handleResult) {
     if (fields.email && !fields.email.match(emailRegexp)) {
         handleResult('Invalid email address.');
         return;
+    }
+    if (files.photo.name && files.photo.type != 'image/jpeg' && files.photo.type != 'image/png') {
+        handleResult('Unsupported image format: ' + files.photo.type);
+        return;
+    }
+
+    if (files.photo.name) {
+        var md5sum = crypto.createHash('md5');
+        md5sum.update(username + '|' + Date.now() + files.photo.name);
+        var randomName = md5sum.digest('hex') + (files.photo.type === 'image/png' ? '.png' : '.jpg');
+
+        // Delete old photo if it exists (we're assuming that random name is unique)
+        client.query('SELECT photo FROM accounts WHERE username = ?', [username], function (err, results, fields) {
+                         if (err) {
+                             throw err;
+                         } else {
+                             fs.unlink('static/photos/' + results[0].photo);
+                         }
+                     });
+
+        // Can't use fs.rename across filesystem boundaries (http://stackoverflow.com/questions/4568689)
+        var is = fs.createReadStream(files.photo.path);
+        var os = fs.createWriteStream('static/photos/' + randomName);
+        util.pump(is, os, function (err) {
+                      fs.unlink(files.photo.path);
+                      if (err) {
+                          throw err;
+                      } else {
+                          client.query('UPDATE accounts SET photo = ? WHERE username = ?', [randomName, username], function (err) {
+                                           if (err) {
+                                               throw err;
+                                           }
+                                       });
+                      }
+                  });
     }
 
     var data = [{field: "firstname", value: fields.firstname || undefined},
@@ -122,10 +163,11 @@ function authenticateUser(username, logUserIn) {
 
 const app = express.createServer();
 app.use(express.limit('1mb'));
-app.use(express.bodyParser());
 app.use(express.static('static'));
 app.use(express.cookieParser());
 app.use(express.session({ secret: "secret string in a public repo ftw!" }));
+app.use(form({ keepExtensions: true }));
+//app.use(express.bodyParser()); // FIXME: re-enable with express 3 which can parse files submitted in form data
 
 app.get('/', function(req, res) {
             res.render('index.ejs', {title: 'index', info: req.flash('info'), error: req.flash('error'),
@@ -146,17 +188,19 @@ app.get('/login', function(req, res) {
         });
 
 app.post('/login', function(req, res) {
-             var username = req.body.username;
-             authenticateUser(username, function (error) {
-                                  if (error) {
-                                      req.flash('error', error);
-                                      res.render('login.ejs', {title: 'login', info: req.flash('info'), error: req.flash('error')});
-                                  } else {
-                                      req.flash("info", username + " logged in");
-                                      req.session.username = username;
-                                      res.redirect('home');
-                                  }
-                              });
+             req.form.complete(function (err, fields, files) {
+                                   var username = fields.username;
+                                   authenticateUser(username, function (error) {
+                                                        if (error) {
+                                                            req.flash('error', error);
+                                                            res.render('login.ejs', {title: 'login', info: req.flash('info'), error: req.flash('error')});
+                                                        } else {
+                                                            req.flash("info", username + " logged in");
+                                                            req.session.username = username;
+                                                            res.redirect('home');
+                                                        }
+                                                    });
+                               });
          });
 
 app.get('/logout', function(req, res) {
@@ -176,14 +220,20 @@ app.get('/profile', function(req, res) {
         });
 
 app.post('/profile', function(req, res) {
-             updateUserProfile(req.session.username, req.body, function (error) {
-                                  if (error) {
-                                      req.flash('error', error);
-                                      res.redirect('/profile');
-                                  } else {
-                                      req.flash("info", req.session.username + " updated");
-                                      res.redirect('home');
-                                  }
+             req.form.complete(function (err, fields, files) {
+                                   if (err) {
+                                       next(err);
+                                   } else {
+                                       updateUserProfile(req.session.username, fields, files, function (error) {
+                                                             if (error) {
+                                                                 req.flash('error', error);
+                                                                 res.redirect('/profile');
+                                                             } else {
+                                                                 req.flash("info", req.session.username + " updated");
+                                                                 res.redirect('home');
+                                                             }
+                                                         });
+                                   }
                                });
          });
 
@@ -192,20 +242,22 @@ app.get('/register', function(req, res) {
         });
 
 app.post('/register', function(req, res) {
-             var username = req.body.username;
-             if (!username) {
-                 res.redirect('home');
-                 return;
-             }
-             createAccount(username, function (error) {
-                               if (error) {
-                                   req.flash('error', error);
-                                   res.render('register.ejs', {title: 'register', info: req.flash('info'), error: req.flash('error')});
-                               } else {
-                                   req.flash("info", username + " user account successfully created");
-                                   res.redirect('home');
-                               }
-                           });
+             req.form.complete(function (err, fields, files) {
+                                   var username = fields.username;
+                                   if (!username) {
+                                       res.redirect('home');
+                                       return;
+                                   }
+                                   createAccount(username, function (error) {
+                                                     if (error) {
+                                                         req.flash('error', error);
+                                                         res.render('register.ejs', {title: 'register', info: req.flash('info'), error: req.flash('error')});
+                                                     } else {
+                                                         req.flash("info", username + " user account successfully created");
+                                                         res.redirect('home');
+                                                     }
+                                                 });
+                               });
          });
 
 app.listen(9001);
