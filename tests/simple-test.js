bd645047426acae8c600ef5655a0f884118c332a
Francois Marier <francois@debian.org>
2012-03-23 14:26:21 -0700

    Working test for the counter library

--- tests/simple-test.js
+++ tests/simple-test.js
@@ -1,18 +1,35 @@
 #!/usr/bin/node
 
 const
+counter = require('../lib/counter.js'),
 assert = require('assert'),
 vows = require('vows');
 
-var suite = vows.describe("simple test");
+var suite = vows.describe("database tests");
 suite.options.error = false;
 
 suite.addBatch({
-  "everything": {
-    topic: function() { return "yes!"; },
-    "works": function(r) {
-      assert.isString(r);
-      assert.equal(r, "yes!");
+  "read the counter": {
+    topic: function() {
+        counter.read(this.callback);
+    },
+      "and it returns a number": function(r) {
+        assert.isNumber(r);
+    }
+  },
+  "bump the counter": {
+    topic: function() {
+        var cb = this.callback;
+        counter.read(function (v1) {
+                         counter.bump(function () {
+                                          counter.read(function (v2) {
+                                                           cb(v1, v2);
+                                                       });
+                                      });
+                     });
+    },
+    "and it works": function(r1, r2) {
+        assert.equal(r2, r1 + 1);
     }
   }
 });
